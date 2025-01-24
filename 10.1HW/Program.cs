7f1b706bfb9c1ee743ea04c12c92eb346f032799
anzwkk <anam89737@gmail.com>
2025-01-25 01:41:27 +0300

    Split the entered first and last name using an array and assigned to a variable

--- 10.1HW/Program.cs
+++ 10.1HW/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введіть ім’я та прізвище: ");
+string input = Console.ReadLine();
+
+string[] parts = input.Split(' ');
+string firstName = parts[0];
+string lastName = parts[0];
