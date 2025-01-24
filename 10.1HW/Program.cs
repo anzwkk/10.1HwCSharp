9656f1176139cbc4ad83799c1111d378dc1b58af
anzwkk <anam89737@gmail.com>
2025-01-25 01:45:58 +0300

    Added check if first name and last name start with the same letter

--- 10.1HW/Program.cs
+++ 10.1HW/Program.cs
@@ -3,4 +3,13 @@ string input = Console.ReadLine();
 
 string[] parts = input.Split(' ');
 string firstName = parts[0];
-string lastName = parts[0];
+string lastName = parts[1];
+
+if (firstName[0] == lastName[0])
+{
+    Console.WriteLine("Прізвище починається на ту ж літеру, що і ім’я");
+}
+else
+{
+    Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім’я");
+}
